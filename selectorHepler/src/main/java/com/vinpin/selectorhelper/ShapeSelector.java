1176804f084439a7bad19f85de04a2f65e2f5bcf
VinPin <zhang.wenping@eisoo.com>
2018-08-02 11:25:00 +0800

    Update ShapeSelector.

--- selectorHepler/src/main/java/com/vinpin/selectorhelper/ShapeSelector.java
+++ selectorHepler/src/main/java/com/vinpin/selectorhelper/ShapeSelector.java
@@ -1,7 +1,8 @@
 package com.vinpin.selectorhelper;
 
 import android.graphics.drawable.GradientDrawable;
-import android.support.annotation.IntDef;
+import android.graphics.drawable.StateListDrawable;
+import android.support.annotation.NonNull;
 
 /**
  * <pre>
@@ -12,33 +13,88 @@ import android.support.annotation.IntDef;
  */
 public class ShapeSelector {
 
-    private static ShapeSelector mShapeSelector;
-    private int mShapeType;
+    private StateListDrawable mSelector;
 
     private ShapeSelector() {
-        mShapeType = GradientDrawable.RECTANGLE;
+        mSelector = new StateListDrawable();
     }
 
     public static ShapeSelector getInstance() {
-        mShapeSelector = new ShapeSelector();
-        return mShapeSelector;
+        return new ShapeSelector();
     }
 
-    @IntDef({GradientDrawable.RECTANGLE, GradientDrawable.OVAL,
-            GradientDrawable.LINE, GradientDrawable.RING})
-    private @interface Shape {
+    public ShapeSelector enabled(boolean enabled, @NonNull GradientDrawable shape) {
+        if (enabled) {
+            mSelector.addState(new int[]{android.R.attr.state_enabled}, shape);
+        } else {
+            mSelector.addState(new int[]{-android.R.attr.state_enabled}, shape);
+        }
+        return this;
+    }
+
+    public ShapeSelector pressed(boolean pressed, @NonNull GradientDrawable shape) {
+        if (pressed) {
+            mSelector.addState(new int[]{android.R.attr.state_pressed}, shape);
+        } else {
+            mSelector.addState(new int[]{-android.R.attr.state_pressed}, shape);
+        }
+        return this;
+    }
+
+    public ShapeSelector selected(boolean selected, @NonNull GradientDrawable shape) {
+        if (selected) {
+            mSelector.addState(new int[]{android.R.attr.state_selected}, shape);
+        } else {
+            mSelector.addState(new int[]{-android.R.attr.state_selected}, shape);
+        }
+        return this;
+    }
+
+    public ShapeSelector checked(boolean checked, @NonNull GradientDrawable shape) {
+        if (checked) {
+            mSelector.addState(new int[]{android.R.attr.state_checked}, shape);
+        } else {
+            mSelector.addState(new int[]{-android.R.attr.state_checked}, shape);
+        }
+        return this;
+    }
+
+    public ShapeSelector checkable(boolean checkable, @NonNull GradientDrawable shape) {
+        if (checkable) {
+            mSelector.addState(new int[]{android.R.attr.state_checkable}, shape);
+        } else {
+            mSelector.addState(new int[]{-android.R.attr.state_checkable}, shape);
+        }
+        return this;
+    }
+
+    public ShapeSelector focused(boolean focused, @NonNull GradientDrawable shape) {
+        if (focused) {
+            mSelector.addState(new int[]{android.R.attr.state_focused}, shape);
+        } else {
+            mSelector.addState(new int[]{-android.R.attr.state_focused}, shape);
+        }
+        return this;
+    }
+
+    public ShapeSelector windowFocused(boolean windowFocused, @NonNull GradientDrawable shape) {
+        if (windowFocused) {
+            mSelector.addState(new int[]{android.R.attr.state_window_focused}, shape);
+        } else {
+            mSelector.addState(new int[]{-android.R.attr.state_window_focused}, shape);
+        }
+        return this;
     }
 
     /**
-     * 设置Shape类型,默认矩形 GradientDrawable.RECTANGLE
+     * 默认的Shape，放在最后生效
      */
-    public ShapeSelector setShape(@Shape int shape) {
-        mShapeType = shape;
+    public ShapeSelector defaultShape(@NonNull GradientDrawable shape) {
+        mSelector.addState(new int[]{}, shape);
         return this;
     }
 
-    public ShapeSelector setSolidColor() {
-
-        return this;
+    public StateListDrawable create() {
+        return mSelector;
     }
 }
